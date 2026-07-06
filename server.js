066af74f1efd19833b61779c54eae65f04ab3dc3
tassoleo5-droid <tassoleo5@gmail.com>
2026-07-06 15:28:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -81,6 +81,10 @@ app.post('/api/trade', authMiddleware, async (req, res) => {
   } catch(e) { res.status(500).json({ error: e.message }); }
 });
 
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'app.html'));
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
